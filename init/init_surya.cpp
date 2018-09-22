38a9c0e120016973d709d40ca57f422a0cf0ad44
TheScarastic <warabhishek@gmail.com>
2018-09-22 03:56:35 -0400

    init: Set PRODUCT_GMS_CLIENTID_BASE as per stock

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -131,7 +131,12 @@ void vendor_load_properties() {
     property_override("ro.build.fingerprint", fp);
     property_override("ro.bootimage.build.fingerprint", fp);
     property_override("ro.system_ext.build.fingerprint", fp);
-    property_override("ro.build.description", "surya_global-user 10 QKQ1.200512.002 V12.0.2.0.QJGMIXM release-keys");
+    property_override("ro.build.description", "surya_global-user 10 QKQ1.200512.002 V12.0.3.0.QJGMIXM release-keys");
+    property_override("ro.com.google.clientidbase", "android-xiaomi");
+    property_override("ro.com.google.clientidbase.ax", "android-xiaomi-rvo3");
+    property_override("ro.com.google.clientidbase.ms", "android-xiaomi-rvo3");
+    property_override("ro.com.google.clientidbase.tx", "android-xiaomi-rvo3");
+    property_override("ro.com.google.clientidbase.vs", "android-xiaomi-rvo3");
 
     property_override("ro.control_privapp_permissions", "log");
 
