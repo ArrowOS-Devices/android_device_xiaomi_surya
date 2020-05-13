d16296ee7974f0454f90922aab7d533b1e08566d
Akhil Narang <me@akhilnarang.dev>
2020-05-13 11:24:39 +0530

    phoenix: actually include overlay-aosip
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- device.mk
+++ device.mk
@@ -85,7 +85,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosip
 
 # Overlays - override vendor ones
 PRODUCT_PACKAGES += \
