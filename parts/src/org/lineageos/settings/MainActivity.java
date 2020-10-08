eda2ce2a5c5f3dc3a9aaad1381b18547127959f5
xyyx <xyyx@mail.ru>
2020-10-08 05:42:37 +0300

    XiaomiParts: fix main screen style and remove divider

--- parts/src/org/lineageos/settings/MainActivity.java
+++ parts/src/org/lineageos/settings/MainActivity.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2016 The NitrogenOS Project
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -15,32 +15,18 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
+
 package org.lineageos.settings;
 
 import android.os.Bundle;
-import androidx.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.view.MenuItem;
 
 public class MainActivity extends PreferenceActivity {
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        addPreferencesFromResource(R.xml.main);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case android.R.id.home:
-            finish();
-            return true;
-        default:
-            break;
-        }
-        return super.onOptionsItemSelected(item);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new MainFragment())
+                .commit();
     }
 }
