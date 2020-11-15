f2e875dbe0246fc0a68684f66494b8070f866434
Lostark13 <tushar9915@gmail.com>
2020-11-15 10:41:21 +0100

    surya: Use Vendor Media Profiles
     The Default Media Profiles Does Not Support Video Record Higher Than 1080p.The Vendor Profile Has Support Upto 4K
    
    Signed-off-by: Lostark13 <tushar9915@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -6,7 +6,8 @@
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam
+    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
