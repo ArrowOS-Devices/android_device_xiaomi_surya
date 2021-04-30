beda9fdef06d74ea580ec53513a12d32d4e72d0d
Ganesh Varma <ganeshvarmai4u@gmail.com>
2021-04-30 23:58:21 +0530

    surya: Block upcoming OTA
    - We'll be moving to OSS builds

--- system_prop.mk
+++ system_prop.mk
@@ -49,3 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += debug.sf.early.sf.duration=21000000
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.early.app.duration=16500000
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.earlyGl.sf.duration=13500000
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.earlyGl.app.duration=21000000
+
+# Block update
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.block_updater=true
