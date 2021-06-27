99e0c9d61efb1fcb7c746e0db36189d17d37f69f
Unicote <arvogl874@gmail.com>
2021-06-27 16:32:30 +0300

    surya: Set prop for for NFC DT
    * Taken from latest MIUI (Xiaomi took this from https://source.codeaurora.org/quic/la/device/qcom/qssi/tree/system.prop?h=LA.QSSI.11.0.r1-12300-qssi.0#n101)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
